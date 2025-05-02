8a6a4313bf4071f3c2d7db468f4abc728d8d9313
Gabriel-Tabajara <gataba.santos@gmail.com>
2025-05-01 23:27:06 -0300

    Add the model for last commit

--- src/models/graph_metadata.rs
+++ src/models/graph_metadata.rs
@@ -12,7 +12,7 @@ impl GraphMetadata {
         GraphMetadata { path, distance, total_time, custom_info }
     }
 
-    pub fn get_distance_from_file(path: String) -> f64 {
+    pub fn get_distance_from_file(path: &String) -> f64 {
         let metadata_string = read_to_string(path).unwrap();
         let split = metadata_string.split("\n");
 
@@ -25,6 +25,21 @@ impl GraphMetadata {
         }
 
         distance
+    }
+
+    pub fn get_time_from_file(path: &String) -> f64 {
+        let metadata_string = read_to_string(path).unwrap();
+        let split = metadata_string.split("\n");
+
+        let mut time = 0.0;
+
+        for line in split {
+            if line.starts_with("Total Time: ") {
+                time = line["Total Time: ".len()..].trim().parse::<f64>().unwrap();
+            }
+        }
+
+        time
     }
 
     pub fn generate_file(&self, path: String) {
