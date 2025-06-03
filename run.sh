44f467f893e93e9e689c8371d5bed427a7d5c1b8
gischenato <gischenato@gmail.com>
2025-06-03 19:45:46 -0300

    run

--- run.sh
+++ run.sh
@@ -1,40 +1,62 @@
 #!/bin/bash
 
-TIMES=15
+TIMES=1
 UFs=(
-    "rr"
-    "ap"
-    "ac"
-    "ro"
-    "am"
-    "se"
-    "es"
-    "ms"
-    "rj"
-    "al"
-    "to"
-    "mt"
-    "pa"
-    "rn"
-    "pe"
-    "ce"
-    "pb"
-    "ma"
-    "pi"
-    "go"
+    # "rr"
+    # "ap"
+    # "ac"
+    # "ro"
+    # "am"
+    # "se"
+    # "es"
+    # "ms"
+    # "rj"
+    # "al"
+    # "to"
+    # "mt"
+    # "pa"
+    # "rn"
+    # "pe"
+    # "ce"
+    # "pb"
+    # "ma"
+    # "pi"
+    # "go"
     "sc"
-    "pr"
-    "ba"
-    "rs"
-    "sp"
-    "mg"
+    # "pr"
+    # "ba"
+    # "rs"
+    # "sp"
+    # "mg"
+    # "brazil"
 )
+
+ALG="ACO"
+VARIANT="candidate_list3" # ou greedy
 
 for uf in "${UFs[@]}"
 do
     for ((i=1; i<=TIMES; i++))
     do
-        echo "Running test $i for UF: $uf"
-        RUSTFLAGS="-Awarnings" cargo run -q -- -u "$uf" -p
+        DIR="src/assets/outputs/$ALG/$uf"
+        FILE="$DIR/${i}_${VARIANT}.txt"
+        
+        mkdir -p "$DIR"  # Cria o diretório se não existir
+    done
+done
+
+for uf in "${UFs[@]}"
+do
+    for ((i=1; i<=TIMES; i++))
+    do
+        DIR="src/assets/outputs/$ALG/$uf"
+        FILE="$DIR/${i}_${VARIANT}.txt"
+        
+        mkdir -p "$DIR"  # Cria o diretório se não existir
+        
+        date > "$FILE"
+        echo "Running test $i for UF: $uf with algorithm: $ALG" | tee -a "$FILE"
+        RUSTFLAGS="-Awarnings" cargo run -q -- -a "$ALG" -u "$uf" -p | tee -a "$FILE"
+        date >> "$FILE"
     done
 done
